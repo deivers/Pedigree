70060dc9eba17e7bee4becaf62e0205df9a5a5db
Doug Ivers <douglasivers@gmail.com>
2015-07-03 11:56:46 -0400

    Add skewedRandomInteger and other functions to utility.js

--- utility.js
+++ utility.js
@@ -1,4 +1,42 @@
 
+if (!exists(Mathy))
+	Mathy = {};
+
+/**
+ * Returns a boolean that is true some percent of the time
+ * @param changeTrue is a ratio (between 0 and 1)
+ */
+ Mathy.skewedRandomBoolean = function(chanceTrue) {
+ 	return (Math.random() < chanceTrue);
+ }
+
+/**
+ * Picks a number according to the statistical frequency array.
+ * For example, given an array {0.1, 0.3, 0.6}, the number will be 0 10% of the time, 1 30% of the time, and 2 60%.
+ * The array need not sum to a value of 1.0.
+ * @param freq array of desired ratios
+ * @return the number
+ */
+Mathy.skewedRandomInteger = function(freqArray) {
+	// assume the elements of freqArray are numbers between 0 and 1
+	if (!exists(freqArray.length))
+		return 0;
+	var r = Math.random();
+	var sum = 0;
+	var accumulator = 0;
+	sum = freqArray.reduce(function(prev,curr) {
+		return prev + curr;
+	});
+	for (var i=0; i<freqArray.length; i++) {
+		accumulator += freqArray[i];
+		if (r <= (accumulator / sum))
+			return i;
+	}
+		return freqArray.length - 1; // never gets here
+}
+
+
+// general utility functions
 
 function debug(x) {
 	if (typeof x === "object" && x != null) {
@@ -24,11 +62,22 @@ jQuery.fn.exists = function(){
 Array.prototype.max = function() {
  return Math.max.apply(null, this);
 };
-// Array.prototype.max = function() {
-// 	var maxVal = this[0];
-// 	this.forEach(function(val){
-// 		maxVal = Math.max(val, maxVal);
-// 	});
-// 	return maxVal;
-// }
+
+function shuffleArray(array) {
+	// Randomize order in-place using Fisher-Yates shuffle algorithm
+	for (var i = array.length - 1; i > 0; i--) {
+		var j = Math.floor(Math.random() * (i + 1));
+		var temp = array[i];
+		array[i] = array[j];
+		array[j] = temp;
+	}
+	return array;
+}
+
+function arrayMake(numberOfElements,multiplier,offset) {
+	var array = new Array(numberOfElements);
+	for (var i=0; i<numberOfElements; i++)
+		array[i] = i * multiplier + offset;
+	return array;
+}
 
